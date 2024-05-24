4d0d1c1923481c932320c584c337b3d5ccc8de2e
Lewi Meyvianus Borosi <lewiyeeey@gmail.com>
2024-05-24 19:49:52 +0700

    Added seeders files
    - categories
    - products
    - brands
    - specs

--- migration/1716477561537_create_table_products.js
+++ migration/1716477561537_create_table_products.js
@@ -2,15 +2,15 @@ module.exports = {
   up: `CREATE TABLE products(
         id SERIAL NOT NULL PRIMARY KEY,
         name VARCHAR(50) NOT NULL,
-        qty INT UNSIGNED NOT NULL DEFAULT 0,
-        imgurl VARCHAR (255) NOT NULL,
+        qty FLOAT UNSIGNED NOT NULL DEFAULT 0,
+        img_url VARCHAR (255) NOT NULL,
         price INT UNSIGNED NOT NULL DEFAULT 0,
         category_id BIGINT UNSIGNED NOT NULL,
         brand_id BIGINT UNSIGNED NOT NULL,
         rating FLOAT NOT NULL  DEFAULT 0,
         description TEXT NOT NULL,
         FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE, 
-        FOREIGN KEY (brand_id) REFERENCES brand(id) ON DELETE CASCADE
+        FOREIGN KEY (brand_id) REFERENCES brands(id) ON DELETE CASCADE
     )`,
   down: "DROP TABLE products",
 };
